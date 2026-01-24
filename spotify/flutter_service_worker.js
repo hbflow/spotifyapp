28fc27f55093b4baec80dd3d523dbe8fdc6fba63
hbflow <hybriidbox@gmail.com>
2026-01-24 10:09:01 -0500

    updated backdrop and spotify website ad overlay

--- spotify/flutter_service_worker.js
+++ spotify/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "dae4641c8944c851b474059d2175bad1",
+const RESOURCES = {"flutter_bootstrap.js": "17c0add639e78fde65a79c28247bb90f",
 "version.json": "dc02b4d7e2115543a9e1f34ff2f9690f",
-"index.html": "358afad41b8b9d83de2b85576126810c",
-"/": "358afad41b8b9d83de2b85576126810c",
-"main.dart.js": "6a26f944f3ed682a0ce36c21521f0f25",
+"index.html": "e948853adc47ed885f3d75cf5d3052a9",
+"/": "e948853adc47ed885f3d75cf5d3052a9",
+"main.dart.js": "e7b95cdf9a4491699ba1752f86da9497",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "720f6d0b58021e41e4708e4cdb67299b",
 "icons/creator2.png": "9306608e0695dd3fad953341f101210e",
